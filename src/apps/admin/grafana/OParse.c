4c36a5f829954574b8699f4ba1969ea07b4542cd
chris hawley <chridye@gmail.com>
2019-05-20 16:15:37 -0400

    [WIP] influx

---
+++ src/apps/admin/grafana/OParse.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define BUFSIZE 4096
+char BUFFER[BUFSIZE];
+
+int main(int argc, char** argv)
+{
+    
+}
---
+++ src/apps/admin/grafana/influxData.c
@@ -0,0 +1,7 @@
+
+
+#define INFLUXSERVER 192.168.122.229
+#define INFLUXPORT 8086 //8086 is the default 
+
+
+
